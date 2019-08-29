df69ab38693d12c7549377d126b42af3fc2b08d9
ChineduOguejiofor <chineduknight@gmail.com>
2019-08-29 08:41:49 +0100

    Updated the links

--- client/js/login.js
+++ client/js/login.js
@@ -1,11 +1,62 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
+const signUpGhost = document.getElementById('signUpGhost');
+const signInGhost = document.querySelector('.ghost');
+const signIn = document.querySelector('#signIn');
+const signUp = document.querySelector('#signUp');
 const container = document.getElementById('container');
+// const url = 'http://localhost:3000/api/auth/signup';
+// const url2 = 'http://localhost:3000/api/auth/login';
+const url = 'http://192.168.43.96:3000/api/auth/signup';
+const url2 = 'http://192.168.43.96:3000/api/auth/login';
 
-signUpButton.addEventListener('click', () => {
+signUpGhost.addEventListener('click', () => {
   container.classList.add('right-panel-active');
 });
 
-signInButton.addEventListener('click', () => {
+signInGhost.addEventListener('click', () => {
   container.classList.remove('right-panel-active');
 });
+
+signIn.addEventListener('click', () => {});
+signUp.addEventListener('click', e => {});
+
+const register = async e => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const name = formData.get('name');
+  const email = formData.get('email');
+  const password = formData.get('password');
+
+  const credentials = { name, email, password };
+  const request = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(credentials),
+    headers: {
+      'content-type': 'application/json'
+    }
+  });
+
+  const data = await request.json();
+  console.log(data);
+};
+
+const loginUser = async e => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const email = formData.get('email');
+  const password = formData.get('password');
+
+  const details = { email, password };
+
+  const request = await fetch(url2, {
+    method: 'POST',
+    body: JSON.stringify(details),
+    headers: {
+      'content-type': 'application/json'
+    }
+  });
+
+  const data = await request.json();
+  console.log(data);
+};
--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
 // const express = require('express');
 import express from 'express';
 import 'dotenv/config';
+import cors from 'cors';
 import db from './routes/api/v1/dbinsert';
 
 const app = express();
 app.use(express.json({ extended: false }));
+app.use(cors());
 
-app.get('/', (req, res) => res.json({ msg: 'API is running' }));
 db.connectDB();
 // Defining routes
 app.use('/api/users/requests', require('./routes/api/v1/users'));
 app.use('/api/auth', require('./routes/api/v1/auth'));
 app.use('/api/requests', require('./routes/api/v1/requests'));
-
+app.use('/', express.static('client'));
 const { PORT } = process.env;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
